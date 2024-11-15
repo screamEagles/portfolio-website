0271a33e7f7325b3bd12dcbe29934912f30409dc
Umair <umairshakeelkhan@gmail.com>
2024-11-15 11:07:08 +0500

    Updated the About Section.

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -48,7 +48,10 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          With over two years of experience in front-end development and content writing, I have successfully contributed to various projects and am well-equipped to handle a range of tasks, from creating dynamic user interfaces to crafting compelling blog posts and social media content. Currently, I serve as the Director of Information Technology at IEEE PES NEDUET, leading hybrid teams with a focus on the MERN Stack. I also hold the position of Deputy Director of Content Writing at The AI Club, where I excel in technical writing, blogging, and social media management. My work with Digtrosoft Technologies Pvt. Ltd. and NCAI NEDUET Smart City Lab has further enhanced my proficiency in GitHub, React.js, and Tailwind CSS. In addition to my professional pursuits, I am passionate about continuous learning, currently studying German, Chinese, and ASL, and enjoy engaging in strategic challenges like chess to sharpen my problem-solving skills.
+          With over two years of experience in front-end development and content writing, I have successfully contributed to various projects and am well-equipped to handle a range of tasks, from creating dynamic user interfaces to crafting compelling blog posts and social media content.<br /><br />
+          I served as the Director of Information Technology at IEEE PES NEDUET, leading hybrid teams with a focus on the MERN Stack, and also held the position of Deputy Director of Content Writing at The AI Club, excelling in writing blogs and social media captions.<br /><br />
+          My work with Digtrosoft Technologies Pvt. Ltd. and NCAI NEDUET Smart City Lab has further enhanced my proficiency in GitHub, React.js, and Tailwind CSS.<br /><br />
+          In addition to my professional pursuits, I am passionate about continuous learning, currently studying German, Chinese, and ASL, and enjoy engaging in strategic challenges like Chess to sharpen my problem-solving skills.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton

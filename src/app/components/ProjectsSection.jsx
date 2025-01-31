84bb9d1234f676bdc123d6170360da8315397405
Umair <umairshakeelkhan@gmail.com>
2025-01-31 12:45:09 +0500

    some basic changes

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -84,7 +84,7 @@ const ProjectsSection = () => {
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+        What I Made
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
